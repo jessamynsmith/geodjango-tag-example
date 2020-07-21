1b7aa795daf83d69da13c28d608d80936f266556
Jessamyn Smith <geekchick77@gmail.com>
2020-07-21 12:59:21 -0600

    Add polygon to map.

--- geodjango_tag/models.py
+++ geodjango_tag/models.py
@@ -1,4 +1,5 @@
 from django.contrib.gis.db import models as gis_models
+from django.contrib.gis.geos import GEOSGeometry
 from django.db import models
 from taggit.managers import TaggableManager
 
@@ -11,6 +12,10 @@ class TaggedLocation(models.Model):
     def tags_text(self):
         tag_list = self.tags.all().values_list('name', flat=True)
         return ', '.join(tag_list)
+
+    def get_location_geo_json(self):
+        geometry = GEOSGeometry(self.location)
+        return geometry.json
 
     def __str__(self):
         return '{}'.format(self.name)
