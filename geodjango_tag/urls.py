9d9dd61ebc363e719a8a2c2a5f62badb3ab8c14c
Jessamyn Smith <geekchick77@gmail.com>
2020-07-21 09:47:10 -0600

    Add detail view with map.

--- geodjango_tag/urls.py
+++ geodjango_tag/urls.py
@@ -5,6 +5,8 @@ from geodjango_tag import views
 
 urlpatterns = [
     path('locations/', views.TaggedLocationListView.as_view(), name='tagged_location_list'),
+    path('locations/<int:pk>/', views.TaggedLocationDetailView.as_view(),
+         name='tagged_location_detail'),
     path('locations/create/', views.TaggedLocationCreateView.as_view(),
          name='tagged_location_create'),
 ]
--- geodjango_tag/views.py
+++ geodjango_tag/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 from django.urls import reverse_lazy
 from geodjango_tag import models as geodjango_tag_models
 
 
 class TaggedLocationListView(ListView):
+    model = geodjango_tag_models.TaggedLocation
+
+
+class TaggedLocationDetailView(DetailView):
     model = geodjango_tag_models.TaggedLocation
 
 
