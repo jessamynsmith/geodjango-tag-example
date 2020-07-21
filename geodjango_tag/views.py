2af6d35da4d52e0e8162adab696061e06396fe8c
Jessamyn Smith <geekchick77@gmail.com>
2020-07-21 10:57:59 -0600

    Order results by num tags matched

--- geodjango_tag/views.py
+++ geodjango_tag/views.py
@@ -1,12 +1,34 @@
+import re
+
 from django.core.serializers import serialize
+from django.db import models
 from django.views.generic import CreateView, DetailView, ListView
 from django.urls import reverse_lazy
-from geodjango_tag import models as geodjango_tag_models
 from jsonview.views import JsonView
+
+from geodjango_tag import models as geodjango_tag_models
 
 
 class TaggedLocationListView(ListView):
     model = geodjango_tag_models.TaggedLocation
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.order_by('name')
+        self.query = self.request.GET.get('query')
+        if self.query:
+            query_args = re.split(r'[,\s]+', self.query)
+            tag_q = models.Q(tags__name__in=query_args)
+            queryset = queryset.filter(tag_q)
+            queryset = queryset.annotate(num_tags=models.Count(
+                'tags', filter=tag_q))
+            queryset = queryset.order_by('-num_tags')
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        context_data['query'] = self.query
+        return context_data
 
 
 class TaggedLocationDetailView(DetailView):
