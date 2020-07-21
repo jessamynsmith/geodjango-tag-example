f7449ee3c4b4bf506ef8d7eb23f5124faeea0ee5
Jessamyn Smith <geekchick77@gmail.com>
2020-07-21 10:37:32 -0600

    Properly show tags in list view

--- geodjango_tag/models.py
+++ geodjango_tag/models.py
@@ -7,3 +7,10 @@ class TaggedLocation(models.Model):
     name = models.CharField(max_length=100)
     location = gis_models.PolygonField()
     tags = TaggableManager()
+
+    def tags_text(self):
+        tag_list = self.tags.all().values_list('name', flat=True)
+        return ', '.join(tag_list)
+
+    def __str__(self):
+        return '{}'.format(self.name)
